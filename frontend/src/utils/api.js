5037773fb1a7bd9e1596fcd14f65650c62290527
Dev-Mjarir08 <08mjarir@gmail.com>
2026-06-21 20:44:40 +0530

    amdox-erp

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/_/backend",
+  baseURL: import.meta.env.VITE_API_URL || "/_/backend",
   withCredentials: true,
 });
 

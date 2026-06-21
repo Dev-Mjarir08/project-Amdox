298925e003fb76272b16b27e88c32de456bdd779
Dev-Mjarir08 <08mjarir@gmail.com>
2026-06-21 17:50:00 +0530

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "",
+  baseURL: "/_/backend",
   withCredentials: true,
 });
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      "/_/backend": {
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/_\/backend/, ""),
       },
     },
   },
